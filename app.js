ae0b21a2e3294d944429cd6895062cf2ccecd54e
thermaldust095 <akhilpendyala69@gmail.com>
2024-02-10 14:33:22 +0530

    https with certificates

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const app = express();
 
+const fs = require('fs');
+const https = require('https');
+
+
 // Parse JSON bodies
 app.use(bodyParser.json());
 
@@ -13,13 +17,21 @@ app.get('/', (req, res) => {
 // Webhook endpoint for receiving messages from WhatsApp
 app.post('/webhook', (req, res) => {
     console.log(req)
-
     // Send a response if needed
     res.status(200).json({ success: true });
 });
 
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+  };
+
+
+const server = https.createServer(options, app);
+
+
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,'0.0.0.0', () => {
+server.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
